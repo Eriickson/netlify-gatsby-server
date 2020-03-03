9b1cd46b8e876f870a710171a8fb214c8a64ccf4
Erickson Manuel <Erickson01d@gmail.com>
2020-03-03 09:38:06 -0500

    Agregando variables de entorno

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import { ApolloServer } from "apollo-server-express";
 import { resolvers } from "./data/resolvers";
 import { typeDefs } from "./data/schemas";
 require("./data/db");
+require("dotenv").config();
 
 const app = express();
-const numeroPuerto = 9000;
+const numeroPuerto = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Plantilla del servidor para Netlify y GatsbyJS");
