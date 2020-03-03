df059ee8dae23ddf0cb95b7483b74b87bc9c151f
Erickson Manuel <Erickson01d@gmail.com>
2020-03-03 08:48:05 -0500

    Agregando url de la base de datos

--- data/db.js
+++ data/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 mongoose.Promise = global.Promise;
 import { AvisoSchema } from "./database";
 
-const url = "mongodb://localhost/github";
+const url =
+  "mongodb+srv://erickson01d:Erickson20@cluster0-q171i.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Aviso = mongoose.model("aviso", AvisoSchema);
